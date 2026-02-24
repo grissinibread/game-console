cad19651a6b4f96c46a786ca974e45353ce71d32
Alejandro Navarro <alexnavarro57@gmail.com>
2026-02-24 01:18:42 -0800

    Updated game tests to use the new constructor.

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,41 +1,48 @@
-import {Game, GameDifficulty} from "./Game.ts";
-import {Player} from "./Player.ts";
+import { Game } from "./Game.ts";
+import { Player } from "./Player.ts";
 
 let game: Game;
+let playerOne: Player;
+let playerTwo: Player;
+
+beforeEach(() => {
+  playerOne = new Player();
+  playerTwo = new Player();
+});
 
 describe("Game difficulty tests.", () => {
     test("If game is set to easy, the number of words for the game should be 3.", () => {
-        let difficulty: GameDifficulty = "EASY";
-        game = new Game(difficulty);
+        let difficulty: string = "1";
+        game = new Game(difficulty, playerOne, playerTwo);
 
         expect(game.numberOfWords).toEqual(3);
     });
 
     test("If game is set to normal, the number of words for the game should be 3.", () => {
-        let difficulty: GameDifficulty = "NORMAL";
-        game = new Game(difficulty);
+        let difficulty: string = "2";
+        game = new Game(difficulty, playerOne, playerTwo);
 
         expect(game.numberOfWords).toEqual(5);
     });
 
     test("If game is set to hard, the number of words for the game should be 3.", () => {
-        let difficulty: GameDifficulty = "HARD";
-        game = new Game(difficulty);
+        let difficulty: string = "3";
+        game = new Game(difficulty, playerOne, playerTwo);
 
         expect(game.numberOfWords).toEqual(7);
     });
 });
 
 test("If current player is two, current player will be changed to one.", () => {
-    let difficulty: GameDifficulty = "EASY";
-    game = new Game(difficulty);
+    let difficulty: GameDifficulty = "1";
+    game = new Game(difficulty, playerOne, playerTwo);
 
     expect(game.currentPlayer === game.playerTwo);
 });
 
 test("If current player is one, current player will be changed to two.", () => {
-    let difficulty: GameDifficulty = "EASY";
-    game = new Game(difficulty);
+    let difficulty: GameDifficulty = "1";
+    game = new Game(difficulty, playerOne, playerTwo);
 
     game.getOpponent();
 
