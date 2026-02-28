e07aff5b73367ce146d2551edbd785c0e29cf024
Alejandro Navarro <alexnavarro57@gmail.com>
2026-02-28 02:42:06 -0800

    Started to add typescript logic to the buttons.

--- client/src/pages/DifficultySelectionPage.tsx
+++ client/src/pages/DifficultySelectionPage.tsx
@@ -3,13 +3,24 @@ function DifficultySelectionPage() {
     <>
       <h2>Select Difficulty</h2>
 
-      <button>Easy</button>
-      <button>Normal</button>
-      <button>Hard</button>
+      <div className="stack">
+        <input id="easy" type="radio" name="difficulty"/>
+        <label htmlFor="easy">Easy</label>
 
-        <button>Create Game</button>
+        <input id="normal" type="radio" name="difficulty"/>
+        <label htmlFor="normal">Normal</label>
+
+        <input id="hard" type="radio" name="difficulty"/>
+        <label htmlFor="hard">Hard</label>
+      </div> 
+
+      <input id="createGameButton" type="button"/>
+      <label htmlFor="createGameButton">Create Game</label>
     </>
   );
 }
 
 export default DifficultySelectionPage;
+
+let button = document.getElementById("createGameButton");
+let clickRadioButton = document.getElementsByName("difficulty");
