fb81b71438a36308fbb7520ab0bbf6b1d82d4ee0
Alejandro Navarro <alexnavarro57@gmail.com>
2026-03-03 01:12:23 -0800

    Added radio button functionality so that the difficulty of the game can be extracted

--- client/src/pages/DifficultySelectionPage.tsx
+++ client/src/pages/DifficultySelectionPage.tsx
@@ -1,26 +1,33 @@
+import { useState } from 'react';
+
 function DifficultySelectionPage() {
+  const [difficulty, enable] = useState<string | null>(null);
+
+  function setDifficulty(difficulty: string) {
+    enable(difficulty);
+
+    return difficulty;
+  }
+
   return (
     <>
       <h2>Select Difficulty</h2>
 
       <div className="stack">
-        <input id="easy" type="radio" name="difficulty"/>
+        <input id="easy" type="radio" name="difficulty" onClick={() => setDifficulty("easy")}/>
         <label htmlFor="easy">Easy</label>
 
-        <input id="normal" type="radio" name="difficulty"/>
+        <input id="normal" type="radio" name="difficulty" onClick={() => setDifficulty("normal")}/>
         <label htmlFor="normal">Normal</label>
 
-        <input id="hard" type="radio" name="difficulty"/>
+        <input id="hard" type="radio" name="difficulty" onClick={() => setDifficulty("hard")}/>
         <label htmlFor="hard">Hard</label>
       </div> 
 
-      <input id="createGameButton" type="button"/>
+      <input id="createGameButton" type="button" disabled={!difficulty} onClick={() => alert(difficulty)}/>
       <label htmlFor="createGameButton">Create Game</label>
     </>
   );
 }
 
 export default DifficultySelectionPage;
-
-let button = document.getElementById("createGameButton");
-let clickRadioButton = document.getElementsByName("difficulty");
